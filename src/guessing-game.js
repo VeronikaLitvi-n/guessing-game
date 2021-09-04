eab1a9e95c97f7fed598a889cb6da693ab6cc9ef
weronika-fed <nikafed3@gmail.com>
2021-09-04 12:27:03 +0300

    feat: implement guessing-game

---
+++ src/guessing-game.js
@@ -0,0 +1,30 @@
+class GuessingGame {
+    constructor() {
+        this.min = null;
+        this.max =null;
+        this.mid = null;
+    }
+
+    setRange(min, max) {
+        if(min > max){
+            return;
+        }
+        this.min = min;
+        this.max = max;
+    }
+
+    guess() {
+        this.mid = Math.round((this.max - this.min)/2) + this.min;
+        return this.mid;
+    }
+
+    lower() {
+        this.max = this.mid;
+    }
+
+    greater() {
+        this.min = this.mid;
+    }
+}
+
+module.exports = GuessingGame;
